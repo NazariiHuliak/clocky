b38fd0fcc0ee2cb0cfd77ac440cbf927895ae2cf
Nazarii Huliak <96128629+NazariiHuliak@users.noreply.github.com>
2025-03-06 19:52:15 +0200

    Sensors pin's data was moved to Config.h

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -14,35 +14,20 @@
 #include <../src/hardware/buttons/ButtonHandler.h>
 #include <hardware/brightness/BrightnessHandler.h>
 
+#include <../src/resources/Config.h>
+
 // Leds
-#define DATA_PIN    A5
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
-#define NUM_LEDS    192
 CRGB leds[NUM_LEDS];
 
-#define BRIGHTNESS         2
-#define FRAMES_PER_SECOND  60
-
 // RTC (real time clock)
-#define RST 21
-#define CLK 10
-#define DAT 20
 ThreeWire myWire(DAT, CLK, RST); // IO, SCLK, CE
 RtcDS1302<ThreeWire> rtc(myWire);
 
 // DS18b20 (temperature)
-#define TEMPERATURE_PIN 6
 OneWire oneWire(TEMPERATURE_PIN);
 DallasTemperature tempSensor(&oneWire);
 
 // Buttons
-#define BUTTON_1 A2
-#define BUTTON_2 8
-#define BUTTON_3 9
-
-#define NUM_BUTTONS 3
-
 const uint8_t buttonPins[] = { BUTTON_1, BUTTON_2, BUTTON_3 };
 ButtonHandler buttonHandler(buttonPins, NUM_BUTTONS);
 BrightnessHandler brightnessHandler(A0);
@@ -54,7 +39,6 @@ WatchFace* watchFaces[4] = {
   new TimerWatchFace(leds, buttonHandler)
 };
 WatchFaceManager watchFaceManager(watchFaces, 4);
-
 
 void setup() {
   // Leds
--- project/src/resources/Config.h
+++ project/src/resources/Config.h
@@ -12,6 +12,28 @@
 #define ICON_HEIGHT 8
 #define ICON_WIDTH 7
 
+// Logs
 #define LOG_ENABLED true
+
+// Leds
+#define DATA_PIN    A5
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+#define NUM_LEDS    192
+#define BRIGHTNESS  2
+
+// RTC (real time clock)
+#define RST 21
+#define CLK 10
+#define DAT 20
+
+// DS18b20 (temperature)
+#define TEMPERATURE_PIN 6
+
+// Buttons
+#define NUM_BUTTONS 3
+#define BUTTON_1 A2
+#define BUTTON_2 8
+#define BUTTON_3 9
 
 #endif
